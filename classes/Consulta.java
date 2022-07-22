cacf409eedc3e0d28452da4eeb13bc9b70f7e74c
sarao3o <sarahferreirah1@gmail.com>
2022-07-22 16:08:25 -0300

    consulta

--- classes/Consulta.java
+++ classes/Consulta.java
@@ -9,5 +9,19 @@ public class Consulta {
     private boolean cancelada; 
     private int cliente; 
 
-    
+    public Consulta(int8 num_consulta, time dia_horario, int profissional, boolean cancelada, int cliente){
+        this.num_consulta = num_consulta;
+        this.dia_horario = dia_horario;
+        this.profissional = profissional;
+        this.cancelada = cancelada;
+        this.cliente = cliente;
+    }
+
+    public int8 getNumConsulta() {
+        return num_consulta;
+    }
+
+    public void setNumConsulta(int8 num_consulta) {
+        this.num_consulta = num_consulta;
+    }
 }
