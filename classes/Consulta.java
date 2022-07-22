1c3b49df75170b7ddee0de1bb8e1f8aa2cdf4785
sarao3o <sarahferreirah1@gmail.com>
2022-07-22 15:51:01 -0300

    um poquim de consulta

---
+++ classes/Consulta.java
@@ -0,0 +1,13 @@
+//package br.ufscar.dc.dsw.domain;
+
+import java.time.LocalDate;
+
+public class Consulta {
+    private int8 num_consulta;
+    private time dia_horario;
+    private int profissional;  
+    private boolean cancelada; 
+    private int cliente; 
+
+    
+}
