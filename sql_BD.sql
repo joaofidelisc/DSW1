4f98c8b003e55fe6d5e1acdca98c5bef7fccdff9
Luiz Filipe <luizfilipe.mattos96@gmail.com>
2022-07-23 10:27:00 -0300

    Versão do banco agora vai

--- sql_BD.sql
+++ sql_BD.sql
@@ -7,9 +7,9 @@ DROP TABLE IF EXISTS administrador;
 CREATE TABLE cliente (
 	email VARCHAR(40) NOT NULL,
 	senha VARCHAR(30) NOT NULL,
-	cpf INT8,
+	cpf BIGINT NOT NULL,
 	nome VARCHAR(50) NOT NULL,
-	telefone INT8,
+	telefone BIGINT,
 	sexo CHAR NOT NULL,
 	data_nascimento DATE,
 	CONSTRAINT PK_CLIENTE PRIMARY KEY (cpf),
@@ -20,20 +20,20 @@ CREATE TABLE cliente (
 CREATE TABLE profissional(
 	email varchar(40) NOT NULL,
 	senha varchar(30) NOT NULL,
-	cpf int8,
+	cpf BIGINT,
 	nome varchar(50) NOT NULL,
-	areaConhecimento char NOT NULL,
-	especialidade char NOT NULL,
+	areaConhecimento VARCHAR(20) NOT NULL,
+	especialidade VARCHAR(20) NOT NULL,
 	CONSTRAINT PK_PROFISSIONAL PRIMARY KEY (cpf)
 );
 
 CREATE TABLE consulta(
-    num_consulta serial,
-	dia DATE,
+    num_consulta SERIAL NOT NULL,
+	data_consulta DATE,
     horario TIME,
-	profissional int NOT NULL,
+	cpf_profissional BIGINT NOT NULL,
+	cpf_cliente BIGINT NOT NULL,
 	cancelada bool DEFAULT FALSE,
-	cliente int NOT NULL,
 	CONSTRAINT PK_CONSULTA PRIMARY KEY (num_consulta),
 	CONSTRAINT FK_CONSULTA_PROFISSIONAL FOREIGN KEY (profissional)
 		REFERENCES profissional(cpf),
@@ -45,5 +45,5 @@ create table IF NOT EXISTS administrador (
 	email VARCHAR (60) NOT NULL,
     senha VARCHAR (24) NOT NULL, 
     nome VARCHAR (50) NOT NULL,
-	CONSTRAINT PK_ADMIN PRIMARY KEY(nome)
+	CONSTRAINT PK_ADMIN PRIMARY KEY(email)
 );
