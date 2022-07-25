2095c8595818772cabe4c962d89095f60c9d98f0
sarao3o <sarahferreirah1@gmail.com>
2022-07-25 15:09:49 -0300

    controller da consulta - insere

--- maven-project/aa1/src/main/java/br/ufscar/dc/dsw/controller/ConsultaController.java
+++ maven-project/aa1/src/main/java/br/ufscar/dc/dsw/controller/ConsultaController.java
@@ -1,7 +1,10 @@
 package br.ufscar.dc.dsw.controller;
 
 import java.io.IOException;
+import java.sql.Time;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -76,11 +79,26 @@ String action = request.getPathInfo();
 		dispatcher.forward(request, response);		
 	}
 
+	/**
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	public void insere(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException{
-				request.setCharacterEncoding("UTF-8");
+		request.setCharacterEncoding("UTF-8");
         
-				int8 num_consulta = request.getParameter("email");
+		Long num_consulta = Long.parseLong(request.getParameter("num_consulta"));
+		Date data_consulta = new SimpleDateFormat("YYYY-MM-dd").parse(request.getParameter("data_consulta"));
+        Time hora_consulta = request.getParameter("hora_consulta");
+		Long cpf_profissional = Long.parseLong(request.getParameter("cpf_profissional"));
+		Long cpf_cliente = Long.parseLong(request.getParameter("cpf_cliente"));
+		boolean cancelada = request.getParameter("cancelada") != null;
+        
+        Consulta consulta = new Consulta(num_consulta, data_consulta, hora_consulta, cpf_profissional, cpf_cliente,cancelada);
+        dao.insert(consulta);
+        response.sendRedirect("consulta/lista.jsp");
 				
 	}
 	
