19ad28bc378924a377c746a073facd9f125ce896
João Vitor Cardozo <82623656+joaofidelisc@users.noreply.github.com>
2022-08-25 17:15:39 -0300

    Delete AA2 directory

--- AA2/src/main/java/br/ufscar/dc/dsw/dao/AdminDAO.java
+++
@@ -1,9 +0,0 @@
-package br.ufscar.dc.dsw.dao;
-
-import br.ufscar.dc.dsw.domain.Administrator;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminDAO extends CrudRepository<Administrator, String> {
-    
-}
