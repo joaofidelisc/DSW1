bfb1f774372306f152c5d02079510d208cccf037
sarao3o <sarahferreirah1@gmail.com>
2022-08-27 15:24:03 -0300

    consulta

---
+++ AA2/src/main/java/br/ufscar/dc/dsw/dao/IConsultaDAO.java
@@ -0,0 +1,21 @@
+package br.ufscar.dc.dsw.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.ufscar.dc.dsw.domain.Cliente;
+import br.ufscar.dc.dsw.domain.Consulta;
+import br.ufscar.dc.dsw.domain.Profissional;
+
+
+public interface IConsultaDAO extends CrudRepository<Consulta, Long> {
+    
+    Consulta findById(long id);
+
+    List<Consulta> findAll();
+    List<Consulta> findbyCliente(Cliente cliente);
+    List<Consulta> findByProfissional(Profissional profissional);
+
+    Consulta agendar(Consulta consulta);
+}
